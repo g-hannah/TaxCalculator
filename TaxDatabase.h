85b4689f65221e4fbff60eb37b3bda58d465c858
gamh86 <g.amh.86@outlook.fr>
2022-04-09 15:45:39 +0100

    add enum class for student loan plans and method declarations

--- TaxDatabase.h
+++ TaxDatabase.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #include <vector>
+#include <map>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include "include/rapidjson/rapidjson.h"
+#include "include/rapidjson/document.h"
+#include "include/rapidjson/stringbuffer.h"
 
 /**
  * A singleton class which handles retrieval and storage
@@ -17,13 +23,26 @@ private:
 
   static TaxDatabase* instance;
   const std::string resourceFile = "resources/income_tax.json";
-  void GetData();
+  void ReadData();
 
   bool haveData = false;
   std::vector<double> incomeTaxBands;
   std::vector<double> incomeTaxRates;
   std::vector<double> nationalInsuranceBands;
   std::vector<double> nationalInsuranceRates;
+
+  enum class TaxDataType
+  {
+    eNIRates,
+    eNIBands,
+    eITRates,
+    eITBands
+  };
+
+  enum class StudentLoanPlan; // forward declaration
+  std::map<StudentLoanPlan, std::pair<double, double>> studentLoansData;
+
+  std::vector<double> GetData(TaxDataType);
 
 public:
   static TaxDatabase* GetInstance()
@@ -40,5 +59,22 @@ public:
   std::vector<double> GetIncomeTaxBands();
   std::vector<double> GetNationalInsuranceRates();
   std::vector<double> GetNationalInsuranceBands();
+
+  using Threshold = double;
+  using Rate = double;
+  std::pair<Threshold,Rate> GetStudentLoanData(StudentLoanPlan);
+  double GetStudentLoanThreshold(StudentLoanPlan);
+  double GetStudentLoanRate(StudentLoanPlan);
+
+  enum class StudentLoanPlan
+  {
+    ePlan1,
+    ePlan2,
+    ePlan4,
+    ePostGrad
+  };
+
+#define StudentLoanThreshold(pair) (pair).first
+#define StudentLoanRate(pair) (pair).second
 };
 
