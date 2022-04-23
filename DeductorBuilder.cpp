654ffdaaae30d403e20b452bbfd20fcda3d86161
gamh86 <g.amh.86@outlook.fr>
2022-04-23 21:00:10 +0100

    rename vars

--- DeductorBuilder.cpp
+++ DeductorBuilder.cpp
@@ -7,39 +7,42 @@ namespace UKTax
     DeductorBuilder* DeductorBuilder::SetRegion(TaxDatabase::UKRegion region)
     {
       this->region = region;
-      gotRegion = true;
+      this->gotRegion = true;
+
       return this;
     }
 
     DeductorBuilder* DeductorBuilder::SetStudentLoanPlan(TaxDatabase::StudentLoanPlan plan)
     {
-      hasStudentLoan = true;
+      this->hasStudentLoan = true;
       this->plan = plan;
+
       return this;
     }
 
-    DeductorBuilder* DeductorBuilder::SetSalary(double salary)
+    DeductorBuilder* DeductorBuilder::SetGross(double gross)
     {
-      if (0.0 >= salary)
+      if (0.0 >= gross)
         throw std::exception();
 
-      this->salary = salary;
-      gotSalary = true;
+      this->gross = gross;
+      this->gotGross = true;
+
       return this;
     }
 
-    std::unique_ptr<BaseDeductor> DeductorBuilder::Build()
+    std::unique_ptr<BaseTaxDeductor> DeductorBuilder::Build()
     {
-      if (!gotSalary || 0.0 >= salary || !gotRegion)
-        throw std::exception();
+      if (!gotGross || 0.0 >= gross || !gotRegion)
+        throw std::exception("Attempting to build a deductor without required set parameters.");
 
-      std::unique_ptr<BaseDeductor> deductor = std::make_unique<NationalInsuranceDeductor>();
-      BaseDeductor* ptr = nullptr;
+      std::unique_ptr<BaseTaxDeductor> deductor = std::make_unique<NationalInsuranceDeductor>(gross, region);
+      BaseTaxDeductor* ptr = nullptr;
 
-      ptr = deductor->SetNext(new IncomeTaxDeductor(region, salary));
+      ptr = deductor->SetNext(new IncomeTaxDeductor(gross, region));
 
       if (hasStudentLoan)
-        ptr->SetNext(new StudentLoanDeductor(plan, salary));
+        ptr->SetNext(new StudentLoanDeductor(gross, plan));
 
       return std::move(deductor);
     }
